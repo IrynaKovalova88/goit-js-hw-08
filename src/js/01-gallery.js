f6d520a3e4dc2b45a637fc24ef1efe3c20ef70f7
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-27 01:44:09 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const imageMarkup = createImageMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imageMarkup);
 
+function createImageMarkup(items) {
+return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a
+        class="gallery__link"
+        href = '${original}';
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+    })
+    .join('');    
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
