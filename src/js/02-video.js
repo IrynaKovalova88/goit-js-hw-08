eea9edc42ccc7a60fa1be5266e87ca9ea81d436e
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-28 22:22:19 +0300

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,12 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const STORAGE_KEY = 'videoplayer-current-time';
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 function onTimePlayer(data) {
-    localStorage.setItem(STORAGE_KEY, data.seconds);
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
 };
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));        
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));        
 
 player.on('timeupdate', throttle(onTimePlayer, 1000));
