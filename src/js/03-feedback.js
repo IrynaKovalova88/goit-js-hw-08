4aa21459716a5c06c0c0fdc718fb82afdb9d4e39
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-28 23:26:47 +0300

    03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+initForm();
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    const formData = new FormData(form);
+    formData.forEach((value, name) => console.log(value, name));
+ }
+
+function onFormInput(e) {
+    let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+  persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
+  persistedFilters[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(persistedFilters));
+}
+
+function initForm() {
+    let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (persistedFilters) {
+    persistedFilters = JSON.parse(persistedFilters);
+    Object.entries(persistedFilters).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }  
+}
