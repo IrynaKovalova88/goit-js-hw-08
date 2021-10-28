5f93078b4b14ca6d3ec5f240058febb4be803314
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-28 21:01:14 +0300

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function onTimePlayer(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).catch(function (error) {
+  console.log(error);
+});        
+
+player.on('timeupdate', throttle(onTimePlayer, 1000));
